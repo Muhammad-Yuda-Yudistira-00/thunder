f3ef03808258682d465cbb31b6f1484f5ea2be2f
Muhammad-Yuda-Yudistira-00 <muhammad_yuda_yudistira@proton.me>
2024-11-02 18:08:10 +0700

    ui(post) fix width post fixed width

--- resources/views/components/general/chat-bubble.blade.php
+++ resources/views/components/general/chat-bubble.blade.php
@@ -1,7 +1,7 @@
 @foreach($posts as $post)
   <div class="flex items-start gap-2.5 mb-4">
      <img class="w-8 h-8 rounded-full" src="{{ asset('img/user-3.png') }}" alt="Jese image">
-     <div class="flex flex-col w-full w-[620px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+     <div class="flex flex-col w-[620px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
         <div class="flex items-center space-x-2 rtl:space-x-reverse">
            <span class="text-sm font-semibold text-gray-900 dark:text-white">{{ $post->user->name }}</span>
            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">{{ $post->updated_at->diffForHumans() }}</span>
